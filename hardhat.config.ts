29052c37799ab808670c935fce6360d79d3d42b4
alonecandies <dragonfrezze@gmail.com>
2023-07-24 14:36:39 +0700

    verify in linea

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,18 +1,37 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-etherscan";
+// import "@nomicfoundation/hardhat-toolbox";
 require("dotenv").config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
   networks: {
-    bsctest: {
-      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+    // bsctest: {
+    //   url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+    //   accounts: [process.env.PRIV_KEY || ""],
+    // },
+    linea: {
+      url: "https://rpc.linea.build",
+      chainId: 59144,
       accounts: [process.env.PRIV_KEY || ""],
     },
   },
-  etherscan:{
-    apiKey: process.env.API_KEY
-  }
+  etherscan: {
+    apiKey: {
+      // bsctest: process.env.BSC_API_KEY || "",
+      linea: process.env.LINEA_API_KEY || "",
+    },
+    customChains: [
+      {
+        network: "linea",
+        chainId: 59144,
+        urls: {
+          apiURL: "https://api.lineascan.build/api",
+          browserURL: "https://lineascan.build/",
+        },
+      },
+    ],
+  },
 };
 
 export default config;
